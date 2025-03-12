a3e6f3e0f725d6eb6dbb03438f3230e02aa14ba9
Tobi Williams <tobiwilliams003@gmail.com>
2025-03-12 22:13:25 +0100

    Add missing FinalCta.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 "use client"
 
-import Hero from "@/components/Hero"
-import BrandPhilosophy from "@/components/BrandPhilosophy"
-import Collection from "@/components/Collection"
-import WhyVelvet from "@/components/WhyVelvet"
-import OrderSection from "@/components/OrderSection"
-import SocialProof from "@/components/SocialProof"
-import FaqSection from "@/components/FaqSection"
-import FinalCta from "@/components/FinalCta"
-import Footer from "@/components/Footer"
-import CustomCursor from "@/components/CustomCursor"
-import ParallaxText from "@/components/ParallaxText"
-
+import { useEffect } from "react"
+import Hero from "./components/Hero"
+import BrandPhilosophy from "./components/BrandPhilosophy"
+import Collection from "./components/Collection"
+import WhyVelvet from "./components/WhyVelvet"
+import OrderSection from "./components/OrderSection"
+import SocialProof from "./components/SocialProof"
+import FaqSection from "./components/FaqSection"
+import FinalCta from "./components/FinalCta"
+import Footer from "./components/Footer"
+import CustomCursor from "./components/CustomCursor"
+import ParallaxText from "./components/ParallaxText"
 
 export default function Home() {
   useEffect(() => {
